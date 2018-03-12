5e9de09563c477b70845ce7a4dc4bb575c95d889
Kaustav Vats <kaustav16048@iiitd.ac.in>
2018-03-12 19:08:38 +0530

    1st Done

--- Assignment-2/121-Chat/TRY/client.c
+++ Assignment-2/121-Chat/TRY/client.c
@@ -28,6 +28,7 @@ int main(int argc, char const *argv[])
   FD_SET(0, &master);
   FD_SET(sockfd, &master);
   size = sockfd;
+  int times = 1;
   printf("\nMenu\n1) To send to a client followed by port number and message.\n2) To send message to all clients.\n3) Exit\n");
   while(1){
 	other = master;
@@ -36,11 +37,13 @@ int main(int argc, char const *argv[])
 	for(i=0; i <= size; i++ )
 	{
 		if(FD_ISSET(i, &other))
-		{
+		{	//printf("\nMenu\n1) To send to a client followed by port number and message.\n2) To send message to all clients.\n");
 			char buffer_out[BUFSIZE];
 			char buffer_in[BUFSIZE];
 			int recieved;
 			if (i == 0){
+				
+				
 				
 				scanf("%s", input);
 				// fgets(input, 2, stdin);
@@ -49,47 +52,34 @@ int main(int argc, char const *argv[])
 				if ( strcmp(input, "1") == 0 )
 				{
 					char  port[10];
-<<<<<<< HEAD
 					// scanf("%s", port);
 					fgets(port, 10, stdin);
 					printf("port: %s\n", port);
 					// scanf("%s", buffer_out);
 					fgets(buffer_out, BUFSIZE, stdin);
 					printf("buffer_out: %s\n", buffer_out);
-=======
-					scanf("%s", port);
-					scanf("%[^\n]", buffer_out);
->>>>>>> 08de66d2818d77a97b3afe0a491997a3ae609ac0
 					send(sockfd, input, sizeof(input), 0);
 					send(sockfd, port, sizeof(port), 0);
 			  		send(sockfd, buffer_out, strlen(buffer_out), 0);
 				}
 				else if ( strcmp(input, "2") == 0 )
 				{
-<<<<<<< HEAD
 					// printf("Enter your message: ");
 					fgets(buffer_out, BUFSIZE, stdin);
 					printf("Your message: %s\n", buffer_out);
 					// scanf("%s", &buffer_out);
-=======
-					// fgets(buffer_out, BUFSIZE, stdin);
-					scanf("%[^\n]", &buffer_out);
->>>>>>> 08de66d2818d77a97b3afe0a491997a3ae609ac0
 					send(sockfd, input, sizeof(input), 0);
 			  		send(sockfd, buffer_out, strlen(buffer_out), 0);
 				}
-				else if( strcmp(input, "3")==0)
+				else if( strcmp(input , "3") == 0)
 				{
 					exit(1);
 				}
 				else{
-<<<<<<< HEAD
 					printf("INCORRECT INPUT\n");
 					// printf("\nMenu\n1) To send to a client followed by port number and message.\n2) To send message to all clients.\n3) Exit");
-=======
-					continue;
->>>>>>> 08de66d2818d77a97b3afe0a491997a3ae609ac0
 				}
+				printf("\nMenu\n1) To send to a client followed by port number and message.\n2) To send message to all clients.\n3) Exit\n");
 			}
 			else {
 				printf("Waiting for clients message.\n");
@@ -100,7 +90,6 @@ int main(int argc, char const *argv[])
 			}
 		}
 	}
-	printf("\nMenu\n1) To send to a client followed by port number and message.\n2) To send message to all clients.\n3) Exit\n");
   }
   printf("client-quited\n");
   close(sockfd);
