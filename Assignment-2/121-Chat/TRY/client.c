a390ea42314dfa8284a841a6db08049efeea818b
Kaustav Vats <kaustav16048@iiitd.ac.in>
2018-03-21 23:41:31 +0530

    v2

--- Assignment-2/121-Chat/TRY/client.c
+++ Assignment-2/121-Chat/TRY/client.c
@@ -1,49 +1,65 @@
+// Kaustav Vats (2016048)
+// Saksham Vohra (2016085)
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+
+#include <netdb.h>
+#include <unistd.h>
+#include <netinet/in.h>
+
+#include <arpa/inet.h>
+
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <errno.h>
+
 #define BUFSIZE 1024
+
 int main(int argc, char const *argv[])
 {
-  int sockfd, size, i;
+  int fd_socket, size;
   struct sockaddr_in server;
-  fd_set master;
+  
+  fd_set main;
   fd_set other;
-  if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-	perror("Socket");
+  
+  if ((fd_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1) 
+  {
+	printf("Socket not creating\n");
 	exit(1);
   }
+  
   server.sin_family = AF_INET;
   server.sin_port = htons(5555);
   server.sin_addr.s_addr = inet_addr("127.0.0.1");
+  
   memset(server.sin_zero, '\0', sizeof server.sin_zero);
-  connect(sockfd, (struct sockaddr *)&server, sizeof(struct sockaddr));
-  FD_ZERO(&master);
+  connect(fd_socket, (struct sockaddr *)&server, sizeof(struct sockaddr));
+  
+  FD_ZERO(&main);
   FD_ZERO(&other);
-  FD_SET(0, &master);
-  FD_SET(sockfd, &master);
-  size = sockfd;
+  
+  FD_SET(0, &main);
+  FD_SET(fd_socket, &main);
+  
+  size = fd_socket;
   int times = 1;
   printf("\nMenu\n1) To send to a client followed by port number and message.\n2) To send message to all clients.\n3) Exit\n");
-  while(1){
-	other = master;
+  while(1)
+  {
+	other = main;
 	select(size+1, &other, NULL, NULL, NULL);
 	char input[2];
-	for(i=0; i <= size; i++ )
+	int i;
+	for ( i=0; i<=size; i++ )
 	{
 		if(FD_ISSET(i, &other))
 		{	//printf("\nMenu\n1) To send to a client followed by port number and message.\n2) To send message to all clients.\n");
 			char buffer_out[BUFSIZE];
 			char buffer_in[BUFSIZE];
 			int recieved;
-			if (i == 0){
-				
-				
+			if (i == 0)
+			{
 				
 				scanf("%s", input);
 				// fgets(input, 2, stdin);
@@ -58,9 +74,10 @@ int main(int argc, char const *argv[])
 					// scanf("%s", buffer_out);
 					fgets(buffer_out, BUFSIZE, stdin);
 					printf("buffer_out: %s\n", buffer_out);
-					send(sockfd, input, sizeof(input), 0);
-					send(sockfd, port, sizeof(port), 0);
-			  		send(sockfd, buffer_out, strlen(buffer_out), 0);
+					
+					send(fd_socket, input, sizeof(input), 0);
+					send(fd_socket, port, sizeof(port), 0);
+			  		send(fd_socket, buffer_out, strlen(buffer_out), 0);
 				}
 				else if ( strcmp(input, "2") == 0 )
 				{
@@ -68,30 +85,33 @@ int main(int argc, char const *argv[])
 					fgets(buffer_out, BUFSIZE, stdin);
 					printf("Your message: %s\n", buffer_out);
 					// scanf("%s", &buffer_out);
-					send(sockfd, input, sizeof(input), 0);
-			  		send(sockfd, buffer_out, strlen(buffer_out), 0);
+					send(fd_socket, input, sizeof(input), 0);
+			  		send(fd_socket, buffer_out, strlen(buffer_out), 0);
 				}
 				else if( strcmp(input , "3") == 0)
 				{
 					exit(1);
 				}
-				else{
+				else
+				{
 					printf("INCORRECT INPUT\n");
 					// printf("\nMenu\n1) To send to a client followed by port number and message.\n2) To send message to all clients.\n3) Exit");
 				}
 				printf("\nMenu\n1) To send to a client followed by port number and message.\n2) To send message to all clients.\n3) Exit\n");
 			}
-			else {
+			else 
+			{
 				printf("Waiting for clients message.\n");
-			  	recieved = recv(sockfd, buffer_in, BUFSIZE, 0);
+			  	
+			  	recieved = recv(fd_socket, buffer_in, BUFSIZE, 0);
 			  	buffer_in[recieved] = '\0';
+			  	
 			  	printf("%s\n" , buffer_in);
 			  	printf("\nMenu\n1) To send to a client followed by port number and message.\n2) To send message to all clients.\n3) Exit\n");
 			}
 		}
 	}
   }
-  printf("client-quited\n");
-  close(sockfd);
+  close(fd_socket);
   return 0;
 }
--- Assignment-2/121-Chat/TRY/server.c
+++ Assignment-2/121-Chat/TRY/server.c
@@ -1,63 +1,97 @@
+// Kaustav Vats (2016048)
+// Saksham Vohra (2016085)
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#include <netdb.h>
 #include <unistd.h>
+#include <netinet/in.h>
+
+#include <arpa/inet.h>
+
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netdb.h>
+
 #define PORT 5555
 #define BUFSIZE 1024
+
 int main(int argc, char const *argv[])
 {
 	int ports[20];
 	int fds[20];
 	int connections = 0;
-	fd_set master;
+	
+	fd_set main;
+	
 	fd_set other;
-	int size, i;
-	int sockfd= 0;
+	int size;
+	
+	int fd_serv= 0;
+	
 	struct sockaddr_in server, client;
-	FD_ZERO(&master);
+	
+	FD_ZERO(&main);
 	FD_ZERO(&other);
+	
 	int yes = 1;
-	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-		perror("Socket");
+	if ((fd_serv = socket(AF_INET, SOCK_STREAM, 0)) == -1) 
+	{
+		printf("Socket not connected\n");
 		exit(1);
 	} 
+
 	server.sin_family = AF_INET;
 	server.sin_port = htons(5555);
 	server.sin_addr.s_addr = INADDR_ANY;
+	
 	memset(server.sin_zero, '\0', sizeof server.sin_zero);
-	bind(sockfd, (struct sockaddr *)&server, sizeof(struct sockaddr));
-	listen(sockfd, 10);
+	
+	bind(fd_serv, (struct sockaddr *)&server, sizeof(struct sockaddr));
+	
+	listen(fd_serv, 10);
+	
 	printf("\nTCPServer Waiting for client on port 5555\n");
-	FD_SET(sockfd, &master);
-	size = sockfd;
-	while(1){
-		other = master;
+	FD_SET(fd_serv, &main);
+	
+	size = fd_serv;
+	while(1)
+	{
+		
+		other = main;
 		select(size+1, &other, NULL, NULL, NULL);
-		for (i = 0; i <= size; i++){
-			if (FD_ISSET(i, &other)){
-				if (i == sockfd)
+		
+		int i;
+		for ( i=0; i<=size; i++ ) 
+		{
+			if (FD_ISSET(i, &other))
+			{
+				if (i == fd_serv)
 				{
-					socklen_t socketlen;
-					int newsockfd;
-					socketlen = sizeof(struct sockaddr_in);
-					if((newsockfd = accept(sockfd, (struct sockaddr *)&client, &socketlen)) == -1) {
-						perror("accept");
+					socklen_t s_len;
+					int newfd_serv;
+					
+					s_len = sizeof(struct sockaddr_in);
+					
+					if((newfd_serv = accept(fd_serv, (struct sockaddr *)&client, &s_len)) == -1) 
+					{
+						printf("socket accepting problem\n");
 						exit(1);
-					}else {
-						FD_SET(newsockfd, &master);
-						if(newsockfd > size){
-							size = newsockfd;
+					}
+					else 
+					{
+						FD_SET(newfd_serv, &main);
+						
+						if(newfd_serv > size)
+						{
+							size = newfd_serv;
 						}
 						ports[connections] = ntohs(client.sin_port);
-						fds[connections] = newsockfd;
+						fds[connections] = newfd_serv;
 						connections++;
+						
 						printf("new connection from port %d \n", ntohs(client.sin_port));
-						printf("NewSockFD: %d\n", newsockfd);
+						printf("Newfd_serv: %d\n", newfd_serv);
 					}
 				}
 				else
@@ -66,6 +100,7 @@ int main(int argc, char const *argv[])
 					char buffer[BUFSIZE], buf[BUFSIZE], buffernew[BUFSIZE];
 					char port[10];
 					char input[2];
+					
 					memset(&buffer[0], '\0', BUFSIZE);
 					memset(&buffernew[0], '\0', BUFSIZE);
 					// char *in = (char *)&input;
@@ -88,18 +123,26 @@ int main(int argc, char const *argv[])
 					if (strcmp(input, "1") == 0 )
 					{
 						printf("Recieved input = 1\n");
+						
 						recv(i, port, sizeof(port), 0);
 						int b = atoi(port);
 						printf("%d\n", b);
-						if ((recieved = recv(i, buffer, BUFSIZE, 0)) <= 0) {
-							if (recieved == 0) {
+						
+						if ((recieved = recv(i, buffer, BUFSIZE, 0)) <= 0) 
+						{
+							if (recieved == 0) 
+							{
 								printf("socket %d hung up\n", i);
-							}else {
-								perror("recv");
+							}
+							else 
+							{
+								printf("Mssg not recieved\n");
 							}
 							close(i);
-							FD_CLR(i, &master);
-						}else { 
+
+							FD_CLR(i, &main);
+						}else 
+						{ 
 							printf("%s\n", buffer);
 							int index = -1;
 							int ij;
@@ -128,7 +171,7 @@ int main(int argc, char const *argv[])
 								//printf("B=%d\nport=%d\n",htons(client.sin_port),ports[ij] );
 								int len = sprintf(buffernew,"%d says: %s",ports[ij], buffer);
 								if (send(fds[index], buffernew, len, 0) == -1) {
-									perror("send");
+									printf("Mssg not sent\n");
 								}
 								// else{
 								// 	send(fds[index],"\nMenu\n1) To send to a client followed by port number and message.\n2) To send message to all clients.\n3) Exit\n",114,0);
@@ -150,20 +193,28 @@ int main(int argc, char const *argv[])
 					}
 					else
 					{
-						if ((recieved = recv(i, buffer, BUFSIZE, 0)) <= 0) {
-							if (recieved == 0) {
+						if ((recieved = recv(i, buffer, BUFSIZE, 0)) <= 0) 
+						{
+							if (recieved == 0) 
+							{
 								printf("socket %d hung up\n", i);
-							}else {
-								perror("recv");
+							}
+							else 
+							{
+								printf("Group Mssg not Recieved\n");
 							}
 							close(i);
-							FD_CLR(i, &master);
+							FD_CLR(i, &main);
 						}
-						else { 
+						else 
+						{ 
 							printf("Messg-2-> %s\n", buffer);
-							for(j = 0; j <= size; j++){
-								if (FD_ISSET(j, &master)){
-									if (j != sockfd && j != i) {
+							for(j = 0; j <= size; j++)
+							{
+								if (FD_ISSET(j, &main))
+								{
+									if (j != fd_serv && j != i) 
+									{
 										int ij;
 										for ( ij=0; ij<connections; ij++ )
 										{
@@ -174,10 +225,12 @@ int main(int argc, char const *argv[])
 										}
 										int len = sprintf(buffernew,"%d says: %s", ports[ij],buffer);
 										// send(j,"\nMenu\n1) To send to a client followed by port number and message.\n2) To send message to all clients.\n3) Exit\n",114,0);
-										if (send(j, buffernew, len, 0) == -1) {
-											perror("send");
+										if (send(j, buffernew, len, 0) == -1) 
+										{
+											printf("Group Mssg not sent\n");
 										}
-										else{
+										else
+										{
 											printf("Sending Menu:\n");
 											// send(j,"\nMenu\n1) To send to a client followed by port number and message.\n2) To send message to all clients.\n3) Exit\n",114,0);
 										}
