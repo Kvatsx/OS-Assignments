9ca1d15c056a904e557f7eb88bd042f791df4839
Kaustav Vats <kaustav16048@iiitd.ac.in>
2018-04-15 17:20:53 +0530

    OS-Assignment 3 Done

--- Assignment-3/bootloader/boot2.asm
+++ Assignment-3/bootloader/boot2.asm
@@ -1,6 +1,8 @@
 bits 16
 org 0x7c00							; Output at this location 0x7c00.
 
+HelloWorld: db "Hello-World!   ", 0
+
 main:
 	mov ax, 0x2401
 	int 0x15						; Enables A20 bit
@@ -38,6 +40,10 @@ pointer:
 CODE_SEG equ gdt_code - gdt_start
 DATA_SEG equ gdt_data - gdt_start
 
+Message: db "Hello-World!   0x14DC090000", 0
+cr: db "0x14DC090000", 0
+
+
 bits 32
 
 main2:
@@ -49,23 +55,75 @@ main2:
 	mov ss, ax
 	mov esi, Message
 	mov ebx, 0xb8000
+	call string
+	mov esi, cr3
+	mov ebx, 0xb8000
+	call string
 
-.print_string:							; Method to print hello world using loop
-			lodsb
-			or al, al
-			jz stop
-			or eax,0x0100
-			mov word [ebx], ax
-			add ebx,2
-			jmp .print_string
+	mov ecx, 0xC0000080
+	rdmsr
+	or eax, 1 << 8
+	wrmsr
+	mov eax, cr0
+	or eax, 1 << 31
+	mov cr0, eax
+
+string:		
+		lodsb
+		or al, al
+		jz stop
+		or eax,0x0100
+		mov word [ebx], ax
+		add ebx,2
+		jmp string
 
 stop:
 	cli
 	hlt
+	call printNL
 
-;bits 64
+printNL:
+		mov al, 0
+		stosb
+		mov ah, 0x0E
+		mov al, 0x0D
+		int 0x10
+		mov al, 0x0A
+		int 0x10
+		ret
 
-Message: db "Hello-World!", 0
+
+bits 64
+
+boot64:
+		cli
+		mov ax, gdt_data
+		mov gs, ax
+		mov ss, ax
+		mov fs, ax
+		mov ds, ax
+		mov es, ax
+		mov rax, 0x1F201F201F201F20
+		mov edi, 0xB8000
+		mov ecx, 500
+		rep stosq
+		mov ebx, HelloWorld
+		call print_func
+;		mov ebx, cr3
+		call print_func
+		hlt
+
+print_func:						
+		lodsb
+		or al, al
+		jz clear
+		or eax,0x0100
+		mov word [ebx], ax
+		add ebx,2
+		jmp print_func
+clear:
+	cli
+	hlt
 
 times 510 - ($-$$) db 0
 dw 0xaa55
